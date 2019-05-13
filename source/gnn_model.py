ee26c97445b359acef9431cb50cd49a57d4c667c
Benjamin Gallusser <gallussb@student.ethz.ch>
2019-05-13 17:04:40 -0400

    fix bug in model saving

--- source/gnn_model.py
+++ source/gnn_model.py
@@ -156,10 +156,13 @@ class GnnModel(torch.nn.Module, ABC):
         Should only be called after the end of a training+validation epoch
         """
         # delete older models
-        load_model_dir = os.path.join(self.config.root_dir, self.config.run_abs_path, self.config.model_dir)
-        checkpoint_versions = [name for name in os.listdir(load_model_dir) if (name.endswith('.tar') and name.startswith('epoch'))]
+        load_model_dir = os.path.join(
+            self.config.root_dir, self.config.run_abs_path, self.config.model_dir)
+        checkpoint_versions = [name for name in os.listdir(load_model_dir) if (
+            name.endswith('.tar') and name.startswith('epoch'))]
         if len(checkpoint_versions) >= 3:
-            os.remove(checkpoint_versions.sort()[0])
+            checkpoint_versions.sort()
+            os.remove(checkpoint_versions[0])
 
         # save the new one
         torch.save({
