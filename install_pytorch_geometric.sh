16e424b19db79205481cc16440c846f748f98387
Benjamin Gallusser <gallussb@student.ethz.ch>
2019-05-22 11:31:06 -0400

    adapt install script to necessary torch-geometric commit

--- install_pytorch_geometric.sh
+++ install_pytorch_geometric.sh
@@ -3,4 +3,5 @@ pip install --no-cache-dir torch-scatter
 pip install --no-cache-dir torch-sparse
 pip install --no-cache-dir torch-cluster
 pip install --no-cache-dir torch-spline-conv 
-pip install torch-geometric
+pip install -e git+https://github.com/rusty1s/pytorch_geometric.git@74c4c3c677d9319fcebbfcb421fd711c321f1afe#egg=torch-geometric --no-cache-dir
+#pip install torch-geometric
