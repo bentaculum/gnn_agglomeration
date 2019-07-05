da5b6819dd974a7387d3e3e78de3f75cbfbaf6e1
Benjamin Gallusser <gallussb@student.ethz.ch>
2019-07-05 18:20:18 +0200

    WIP remodel config parsing with ConfigArgParser, difficult to include my 'latest' loading mechansim

--- gnn_agglomeration/config.py
+++ gnn_agglomeration/config.py
@@ -1,5 +1,6 @@
 import torch
 import argparse
+import configargparse
 import os
 import datetime
 import logging
@@ -8,7 +9,7 @@ import pytz
 from operator import attrgetter
 
 
-class SortingHelpFormatter(argparse.HelpFormatter):
+class SortingHelpFormatter(configargparse.HelpFormatter):
     """
     sort arguments when listed on the command line via --help option
     """
@@ -19,70 +20,58 @@ class SortingHelpFormatter(argparse.HelpFormatter):
 
 class Config:
     def __init__(self):
-        self.parser = argparse.ArgumentParser(
+        self.parser = configargparse.ArgumentParser(
+            default_config_file=['./config.ini'],
             formatter_class=SortingHelpFormatter)
-        self.default = {}
 
-        self.parser.add_argument(
-            '--config_from_file',
-            type=str,
-            help="absolute path to config file to be used (json format)")
-        self.default['config_from_file'] = None
+        self.parser.add_argument('--config_file', is_config_file=True,
+            help='file path to config that overwrites the default configs')
 
         self.parser.add_argument(
             '--nodes',
             type=positive_int,
             help='Number of nodes in the graph')
-        self.default['nodes'] = 100
 
         self.parser.add_argument(
             '--self_loops',
             type=str2bool,
             help='whether the random graph should have reflexive edges')
-        self.default['self_loops'] = False
 
         self.parser.add_argument(
             '--euclidian_dimensionality',
             type=positive_int,
             help='Dimension of the Euclidian space, used in data.pos')
-        self.default['euclidian_dimensionality'] = 2
 
         self.parser.add_argument(
             '--feature_dimensionality',
             type=positive_int,
             help='Dimension of the feature space, used in data.x')
-        self.default['feature_dimensionality'] = 6
 
         self.parser.add_argument(
             '--pseudo_dimensionality',
             type=positive_int,
             help='Dimension of the pseudo coordinates, according to their type')
-        self.default['pseudo_dimensionality'] = 3
 
         self.parser.add_argument(
             '--kernel_size',
             type=positive_int,
             help='Attention: # of heads, Splines: # of control points, GMM: # of mixture components')
-        self.default['kernel_size'] = 2
 
         self.parser.add_argument(
             '--data_transform',
             type=str,
             choices=['Cartesian', 'LocalCartesian', 'Distance', 'Polar'],
             help='define the edge attributes (pseudo coordinates)')
-        self.default['data_transform'] = 'Cartesian'
 
         self.parser.add_argument(
             '--theta_max',
             type=float,
             help='nodes with lower euclidian distance will be connected')
-        self.default['theta_max'] = 0.2
 
         self.parser.add_argument(
             '--theta',
             type=float,
             help='euclidian neighborhood distance')
-        self.default['theta'] = 0.1
 
         self.parser.add_argument(
             '--dataset_type',
@@ -90,336 +79,282 @@ class Config:
             choices=['DiameterDataset',
                      'CountNeighborsDataset', 'IterativeDataset', 'HemibrainDataset'],
             help='choose from different types of local datasets')
-        self.default['dataset_type'] = 'HemibrainDataset'
 
         self.parser.add_argument(
             '--dataset_path',
             type=str,
             help='the directory to read the Dataset from')
-        self.default['dataset_path'] = 'data/example_latest'
 
         self.parser.add_argument(
             '--run_path',
             type=str,
             help='directory to save temporary outputs')
-        self.default['run_path'] = 'runs'
 
         self.parser.add_argument(
             '--summary_dir',
             type=str,
             help='relative directory to save temporary summary')
-        self.default['summary_dir'] = 'summary'
 
         self.parser.add_argument(
             '--write_summary',
             type=str2bool,
             help='option to use tensorboardx to monitor the training')
-        self.default['write_summary'] = True
 
         self.parser.add_argument(
             '--log_only_gradients',
             type=str2bool,
             help='whether to write gradients to tensorboard')
-        self.default['log_only_gradients'] = False
 
         self.parser.add_argument(
             '--log_histograms',
             type=str2bool,
             help='whether to perform the costly plotting of histograms')
-        self.default['log_histograms'] = False
 
         self.parser.add_argument(
             '--log_per_epoch_only',
             type=str2bool,
             help='minimal logging, only the loss and the metric')
-        self.default['log_per_epoch_only'] = False
 
         self.parser.add_argument(
             '--log_namespaces',
             type=str,
             nargs='+',
             help='If you want to log only specific namespaces (e.g. layers), specify them here')
-        self.default['log_namespaces'] = []
 
         self.parser.add_argument(
             '--model_dir',
             type=str,
             help='relative directory to save temporary model, for both training and inference')
-        self.default['model_dir'] = 'model'
 
         self.parser.add_argument(
             '--plot_error_locations',
             action='store_true',
             help='if passed, the plot errors by location will be created')
-        self.default['plot_error_locations'] = False
 
         self.parser.add_argument(
             '--plot_graphs_testset',
             type=int,
             help='how many graphs to plot from the test set for visual inspection. -1 means all')
-        self.default['plot_graphs_testset'] = 50
 
         self.parser.add_argument(
             '--validation_split',
             type=unit_float,
             help='define size of validation set, 0 <= ... <= 1')
-        self.default['validation_split'] = 0.1
 
         self.parser.add_argument(
             '--test_split',
             type=unit_float,
             help='define size of test set, 0 <= ... <= 1')
-        self.default['test_split'] = 0.1
 
         self.parser.add_argument(
             '--model',
             type=str,
             help='OurConvModel | GcnModel | GmmConvModel | etc.')
-        self.default['model'] = 'OurConvModel'
 
         self.parser.add_argument(
             '--model_type',
             type=str,
             choices=['ClassificationProblem', 'RegressionProblem'],
             help='ClassificationProblem | RegressionProblem')
-        self.default['model_type'] = 'ClassificationProblem'
 
         self.parser.add_argument(
             '--training_epochs',
             type=positive_int,
             help='number of training epochs')
-        self.default['training_epochs'] = 200
 
         self.parser.add_argument(
             '--samples',
             type=positive_int,
             help='Number of random graphs to create, if a new dataset is created')
-        self.default['samples'] = 100
 
         self.parser.add_argument(
             '--standardize_targets',
             type=str2bool,
             help='targets to mean 0, std 1, if Regression is performed')
-        self.default['standardize_targets'] = True
 
         self.parser.add_argument(
             '--non_linearity',
             type=str,
             help='Activation function from torch.nn.functional, used for hidden layers, e.g. relu | sigmoid | tanh')
-        self.default['non_linearity'] = 'leaky_relu'
 
         self.parser.add_argument(
             '--batch_size_train',
             type=positive_int,
             help='batch size for training')
-        self.default['batch_size_train'] = 8
 
         self.parser.add_argument(
             '--batch_size_eval',
             type=positive_int,
             help='batch size for evaluation')
-        self.default['batch_size_eval'] = 8
 
         self.parser.add_argument(
             '--hidden_layers',
             type=nonnegative_int,
             help='number of hidden layers')
-        self.default['hidden_layers'] = 0
 
         self.parser.add_argument(
             '--hidden_units',
             type=positive_int,
             nargs='+',
             help='number of units per hidden layer in the GNN')
-        self.default['hidden_units'] = [1]
 
         self.parser.add_argument(
             '--use_bias',
             type=str2bool,
             help='whether to use an additive bias')
-        self.default['use_bias'] = True
 
         self.parser.add_argument(
             '--fc_bias',
             type=str2bool,
             help='whether to use a bias term for the final fully connected layer')
-        self.default['fc_bias'] = False
 
         self.parser.add_argument(
             '--fc_layers',
             type=positive_int,
             help='number of fully connected layers in the end, at least 1')
-        self.default['fc_layers'] = 1
 
         self.parser.add_argument(
             '--fc_layer_dims',
             type=positive_int,
             nargs='*',
             help='list of hidden layer dims for fc layers in the end')
-        self.default['fc_layer_dims'] = []
 
         self.parser.add_argument(
             '--fc_dropout_probs',
             type=unit_float,
             nargs='*',
             help='fc network: dropout probs')
-        self.default['fc_dropout_probs'] = []
 
         self.parser.add_argument(
             '--fc_batch_norm',
             type=str2bool,
             help='whether to use Batch Normalization in the final fully connected layer')
-        self.default['batch_norm'] = True
 
         self.parser.add_argument(
             '--dropout_type',
             type=str,
             choices=['dropout', 'dropout2d'],
             help='dropout | dropout2d')
-        self.default['dropout_type'] = 'dropout'
 
         self.parser.add_argument(
             '--dropout_probs',
             type=unit_float,
             nargs='+',
             help='dropout probabilites during training for the input layer and all the hidden layers')
-        self.default['dropout_probs'] = [0.0]
 
         self.parser.add_argument(
             '--adam_lr',
             type=unit_float,
             help='Learning rate for ADAM optimizer')
-        self.default['adam_lr'] = 0.0001
 
         self.parser.add_argument(
             '--adam_weight_decay',
             type=unit_float,
             help='Weight decay for ADAM optimizer')
-        self.default['adam_weight_decay'] = 0.0001
 
         self.parser.add_argument(
             '--batch_norm',
             type=str2bool,
             help='whether to use Batch Normalization')
-        self.default['batch_norm'] = True
 
         self.parser.add_argument(
             '--clip_grad',
             type=str2bool,
             help='whether to use gradient clipping')
-        self.default['clip_grad'] = True
 
         self.parser.add_argument(
             '--clip_value',
             type=float,
             help='gradients are clipped at this value')
-        self.default['clip_value'] = 0.1
 
         self.parser.add_argument(
             '--clip_method',
             type=str,
             choices=['value', 'inf', '1', '2'],
             help='gradient clipping per value or L_-norm')
-        self.default['clip_method'] = 'value'
 
         self.parser.add_argument(
             '--att_heads_concat',
             type=str2bool,
             help='whether to concat or average the outputs of the different attention heads')
-        self.default['att_heads_concat'] = True
 
         self.parser.add_argument(
             '--att_final_dropout',
             type=unit_float,
             help='Dropout probability for the final attention vector')
-        self.default['att_final_dropout'] = 0.0
 
         self.parser.add_argument(
             '--att_layers',
             type=positive_int,
             help='Attention NN: number of layers')
-        self.default['att_layers'] = 1
 
         self.parser.add_argument(
             '--att_layer_dims',
             type=positive_int,
             nargs='+',
             help='Attention NN: list of layer dimensions')
-        self.default['att_layer_dims'] = [1]
 
         self.parser.add_argument(
             '--att_non_linearity',
             type=str,
             help='Attention NN: torch.nn.functional non linearity to use e.g. relu')
-        self.default['att_non_linearity'] = 'leaky_relu'
 
         self.parser.add_argument(
             '--att_batch_norm',
             type=str2bool,
             help='Attention NN: whether to use batch normalization')
-        self.default['att_batch_norm'] = True
 
         self.parser.add_argument(
             '--att_dropout_probs',
             type=unit_float,
             nargs='+',
             help='Attention NN: dropout probabilites during training for the input layer and all the hidden layers')
-        self.default['att_dropout_probs'] = [0.0]
 
         self.parser.add_argument(
             '--att_bias',
             type=str2bool,
             help='Attention NN: option to use bias')
-        self.default['att_bias'] = True
 
         self.parser.add_argument(
             '--att_normalize',
             type=str2bool,
             help='whether to use a softmax over each neighborhood')
-        self.default['att_normalize'] = True
 
         self.parser.add_argument(
             '--att_nodenet_layers',
             type=positive_int,
             help='number of fc layers before the attention mechanism')
-        self.default['att_nodenet_layers'] = 1
 
         self.parser.add_argument(
             '--att_nodenet_hidden_dims',
             type=positive_int,
             nargs='*',
             help='hidden dimensionality of nodenet')
-        self.default['att_nodenet_hidden_dims'] = []
 
         self.parser.add_argument(
             '--load_model',
             type=str,
             help="Load model from file. 'latest' | relative/path/to/tarfile")
-        self.default['load_model'] = None
 
         self.parser.add_argument(
             '--telegram',
             type=str2bool,
             help='whether to have a Sacred Telegram Observer')
-        self.default['telegram'] = True
 
         self.parser.add_argument(
             '--confusion_matrix_path',
             type=str,
             help='file name of confusion matrix')
-        self.default['confusion_matrix_path'] = 'confusion_matrix_test.png'
 
         self.parser.add_argument(
             '--temp',
             type=str2bool,
             help='If true, save results to temp folder. If false, create timestamped directory.')
-        self.default['temp'] = False
 
         self.parser.add_argument(
             '--checkpoint_interval',
             type=positive_int,
             help='how often to save a checkpoint of the model that can be used for restarting')
-        self.default['checkpoint_interval'] = 10
 
         self.parser.add_argument(
             '--machine',
@@ -428,37 +363,31 @@ class Config:
                 'localhost',
                 'slowpoke1'],
             help='machine-dependent parameters to be imported, e.g. for connecting to the MongoDB')
-        self.default['machine'] = 'localhost'
 
         self.parser.add_argument(
             '--msts',
             type=positive_int,
             help='How many different classes in an instance of DiameterGraph')
-        self.default['msts'] = 5
 
         self.parser.add_argument(
             '--class_noise',
             type=unit_float,
             help='how much probability mass is spread between the wrong target classes')
-        self.default['class_noise'] = 0.2
 
         self.parser.add_argument(
             '--class_label_feature',
             type=str2bool,
             help='whether to use the noisy class labels as a feature')
-        self.default['class_label_feature'] = True
 
         self.parser.add_argument(
             '--affinity_dist_alpha',
             type=float,
             help='alpha value for the beta dist that generates noisy edge features')
-        self.default['affinity_dist_alpha'] = 1
 
         self.parser.add_argument(
             '--affinity_dist_beta',
             type=float,
             help='beta value for the beta dist that generates noisy edge features')
-        self.default['affinity_dist_beta'] = 4
 
         self.parser.add_argument(
             '--affinities',
@@ -466,26 +395,29 @@ class Config:
             choices=['all_affinities', 'only_gt_affinities',
                      'only_gt_dir_affinities'],
             help='what kind of affinities on the edges')
-        self.default['affinities'] = 'only_gt_affinities'
 
         self.parser.add_argument(
             '--curvature_degree_limit',
             type=positive_int,
             help='Restrict the deviation from the continuing line by this amount of degrees, in both dirs')
-        self.default['curvature_degree_limit'] = 45
 
         self.parser.add_argument(
             '--edge_labels',
             type=str2bool,
             help='whether to use edge labels')
-        self.default['edge_labels'] = False
 
         self.parser.add_argument(
             '--fc_use_edge',
             type=str2bool,
             help='whether to use the edge information for the final fc layer')
-        self.default['fc_use_edge'] = True
 
+        self.parser.add_argument(
+            '--use_latest_config',
+            action='store_true',
+            help='use custom .json loader to re-use the latest config file'
+        )
+
+    # TODO move this to separate config files
     def localhost(self):
         return {
             'mongo_url': 'localhost:27017',
