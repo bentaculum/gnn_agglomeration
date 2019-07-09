60a70fe7066b89bdcbdb559c71c643760b06fa11
Benjamin Gallusser <gallussb@student.ethz.ch>
2019-07-09 09:16:59 -0400

    update neuroglancer script to scale pyramid for BE frags

--- gnn_agglomeration/dataset/visualization/hemi_evaluate_updated.py
+++ gnn_agglomeration/dataset/visualization/hemi_evaluate_updated.py
@@ -40,11 +40,11 @@ mtlsd_segmentation_benjamin = [
     daisy.open_ds(output_file, f'volumes/segmentation_benjamin/s{i}') for i in range(9)
 ]
 
-# mtlsd_frags_best_effort = [
-# daisy.open_ds(output_file, f'volumes/fragment_best_effort/s{i}') for i in range(9)
-# ]
-mtlsd_frags_best_effort = daisy.open_ds(
-    output_file, 'volumes/fragments_gt_best_effort')
+mtlsd_frags_best_effort = [
+    daisy.open_ds(output_file, f'volumes/fragments_gt_best_effort/s{i}') for i in range(9)
+]
+# mtlsd_frags_best_effort = daisy.open_ds(
+# output_file, 'volumes/fragments_gt_best_effort')
 
 # mtlsd_seg = [
 # daisy.open_ds(f, 'volumes/segmentation_64/s%d' % s)
