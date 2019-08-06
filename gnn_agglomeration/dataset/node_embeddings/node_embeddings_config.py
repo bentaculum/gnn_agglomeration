5ef833b7c74d6dd6edb3291c1c40bc8f9c8bca77
Benjamin Gallusser <gallussb@student.ethz.ch>
2019-08-05 23:03:42 -0400

    WIP add streamed dataset for siamese network

--- gnn_agglomeration/dataset/node_embeddings/node_embeddings_config.py
+++ gnn_agglomeration/dataset/node_embeddings/node_embeddings_config.py
@@ -2,9 +2,32 @@ import configargparse
 import configparser
 import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise configargparse.ArgumentTypeError('Boolean value expected.')
+
+
 p = configargparse.ArgParser(
     default_config_files=['config.ini'])
 
+p.add('--config_file', is_config_file=True,
+      help='file path to config that overwrites the default configs')
+
+p.add('--num_workers', type=int, help='number of workers for torch DataLoader')
+p.add('--samples', type=int, help='number of rag nodes to use for building dataset')
+p.add('--patch_size', type=int, nargs=3, help='3D size to use for creating samples in nanometers')
+p.add('--raw_channel', type=str2bool, help='if set true, create a channel with raw volumetric patches')
+p.add('--mask_channel', type=str2bool, help='if set true, create a channel with binary mask of the fragment in a patch')
 
 config = p.parse_args()
 logging.info(f"\n{p.format_values()}")
--- gnn_agglomeration/dataset/node_embeddings/siamese_dataset.py
+++ gnn_agglomeration/dataset/node_embeddings/siamese_dataset.py
@@ -1,10 +1,12 @@
 import logging
 import torch
 import daisy
+import time
+import numpy as np
 
+from gnn_agglomeration import utils
 # dataset configs for all many params
 from ..config import config
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -14,7 +16,8 @@ class SiameseDataset(torch.utils.data.Data):
     """
     Each data point is actually a mini-batch of volume pairs
     """
-    def __init__(self, length, patch_size, raw_channel, mask_channel,):
+
+    def __init__(self, length, patch_size, raw_channel, mask_channel):
         """
 
         Args:
@@ -29,13 +32,57 @@ class SiameseDataset(torch.utils.data.Data):
         self.mask_channel = mask_channel
         assert raw_channel or mask_channel
 
-        # connect to one RAG DB
+        # TODO parametrize the used names
+        self.id_field = 'id'
+        self.node1_field = 'u'
+        self.node2_field = 'v'
 
-        # get all nodes
-        # get all edges, with gt merge score, as dict of numpy arrays
+        # connect to one RAG DB
+        graph_provider = daisy.persistence.MongoDbGraphProvider(
+            db_name=config.db_name,
+            host=config.db_host,
+            mode='r',
+            nodes_collection=config.nodes_collection,
+            edges_collection=config.edges_collection,
+            endpoint_names=['u', 'v'],
+            position_attribute=[
+                'center_z',
+                'center_y',
+                'center_x'])
+
+        # get all edges, including gt_merge_score, as dict of numpy arrays
+        start = time.time()
+        roi = daisy.Roi(offset=config.roi_offset, shape=config.roi_shape)
+        nodes = graph_provider.read_nodes(roi=roi)
+        edges = graph_provider.read_edges(roi=roi, nodes=nodes)
+        self.nodes_attrs = utils.to_np_arrays(nodes)
+        self.edges_attrs = utils.to_np_arrays(edges)
+        logger.info(f'read whole graph in {time.time() - start}s')
 
     def __len__(self):
         return self.len
+
+    def get_patch(self, center, node_id):
+        offset = np.array(center) - np.array(self.patch_size)/2
+        roi = daisy.Roi(offset=offset, shape=self.patch_size)
+
+        channels = []
+        if self.raw_channel:
+            ds = daisy.open_ds(config.groundtruth_zarr, 'volumes/raw/s0')
+            if not ds.roi.contains(roi):
+                logger.warning(f'location {center} is not fully contained in dataset')
+            patch = (ds[roi].to_ndarray()/255.0).astype(np.float32)
+            channels.append(patch)
+
+        if self.mask_channel:
+            ds = daisy.open_ds(config.fragments_zarr, config.fragments_ds)
+            if not ds.roi.contains(roi):
+                logger.warning(f'location {center} is not fully contained in dataset')
+            patch = ds[roi].to_ndarray()
+            mask = (patch == node_id).astype(np.float32)
+            channels.append(mask)
+
+        return torch.tensor(channels, dtype=torch.float)
 
     def __getitem__(self, index):
         """
@@ -48,8 +95,33 @@ class SiameseDataset(torch.utils.data.Data):
             a mini-batch of volume pairs
 
         """
-        # get all neighbors of random node in RAG
-        # get the raw data for each neighbor, with daisy, using Nils's synful script
+        # pick random node
+        index = np.random.randint(0, len(self.nodes_attrs[self.id_field]))
+        node_id = self.nodes_attrs[self.id_field][index]
+        node_center = (self.nodes_attrs['center_z'][index], self.nodes_attrs['center_y'][index], self.nodes_attrs['center_x'][index])
+        # get raw data for the node
+        node_patch = self.get_patch(center=node_center)
 
-        #
-        pass
+        # get all neighbors of random node in RAG, considering edges in both `directions`
+        neighbor_mask1 = self.edges_attrs[self.node1_field] == node_id
+        neighbors1 = self.edges_attrs[self.node2_field][neighbor_mask1]
+        neighbor_mask2 = self.edges_attrs[self.node2_field] == node_id
+        neighbors2 = self.edges_attrs[self.node1_field][neighbor_mask2]
+
+        neighbor_ids = np.unique(np.concatenate((neighbors1, neighbors2)))
+        neighbor_indices = np.in1d(self.nodes_attrs[self.id_field], neighbor_ids).nonzero()[0]
+        # TODO parametrize
+        centers = [
+            (self.nodes_attrs['center_z'][i], self.nodes_attrs['center_y'][i], self.nodes_attrs['center_x'][i])
+            for i in neighbor_indices
+        ]
+
+        # get the raw data for each neighbor, with daisy, using Nils's synful script
+        patches = []
+        for c, i in zip(centers, neighbor_ids):
+            patches.append(self.get_patch(center=c, node_id=i))
+
+        # TODO make pairs of data, plus corresponding labels
+        # use torch Data class
+
+        return None
