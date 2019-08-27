834465bf2199dbe4377b9acb4ef0394ecc75938a
Benjamin Gallusser <gallussb@student.ethz.ch>
2019-08-27 01:41:52 -0400

    adapt run dir to grid search

--- gnn_agglomeration/config.py
+++ gnn_agglomeration/config.py
@@ -131,7 +131,7 @@ class Config:
             '--run_path',
             type=str,
             help='directory to save temporary outputs')
-        self.default['run_path'] = 'runs'
+        self.default['run_path'] = 'runs/grid_search'
 
         self.parser.add_argument(
             '--summary_dir',
