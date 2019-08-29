8090f21e956d12730da9fad58cfcbba7022f48ab
Benjamin Gallusser <gallussb@student.ethz.ch>
2019-08-29 15:04:46 -0400

    switch to using uint64 in out volume

--- gnn_agglomeration/dataset/visualization/relabel_fragments_with_embeddings.py
+++ gnn_agglomeration/dataset/visualization/relabel_fragments_with_embeddings.py
@@ -5,11 +5,13 @@ import daisy
 from funlib.segment.arrays import replace_values
 import zarr
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+from sklearn.preprocessing import minmax_scale
 from time import time as now
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 def parse_args():
@@ -25,9 +27,11 @@ def parse_args():
     p.add('--chunks_size', type=int, action='append',
           help='zarr array chunk size in voxels. Can be 1d if isotropic or explicitly 3d')
     p.add('--num_workers', type=int)
-    p.add('--out_ds', type=int)
+    p.add('--out_ds', type=str)
 
-    return p.parse_args()
+    config = p.parse_args()
+    logger.info(f'\n{p.format_values()}')
+    return config
 
 
 def load_embeddings(path):
@@ -36,9 +40,12 @@ def load_embeddings(path):
 
 
 def pca(embeddings):
+    # mean 0, variance 1
+    embeddings = scale(embeddings)
     pca = PCA(n_components=3)
     embeddings_3d = pca.fit_transform(embeddings)
-    logger.info(f'PCA explained variance ratio {pca.explained_variance_ratio_}')
+    logger.info(
+        f'PCA explained variance ratio {pca.explained_variance_ratio_}')
     return embeddings_3d
 
 
@@ -46,12 +53,14 @@ def relabel_block(block, fragments, out_volume, node_ids, embeddings):
     start = now()
     fragments = fragments.to_ndarray(block.write_roi)
     logger.info(f'copy fragments into memory in {now() - start} s')
+    logger.debug(f'fragments shape {fragments.shape}')
 
-    out_channels = [np.zeros_like(fragments, dtype=np.float32) for _ in range(3)]
-    out_block = np.stack(out_channels)
+    out_channels = [np.zeros_like(fragments, dtype=np.uint64)
+                    for _ in range(3)]
+    logger.debug(f'out_block channel shape {out_channels[0].shape}')
 
     # force C++ implementation
-    for i, c in enumerate(out_block):
+    for i, c in enumerate(out_channels):
         c = replace_values(
             in_array=fragments,
             out_array=c,
@@ -60,15 +69,21 @@ def relabel_block(block, fragments, out_volume, node_ids, embeddings):
             inplace=False
         )
 
+    # present_ids = np.unique(fragments)
+    # for i, c in enumerate(out_channels):
+    # for j in present_ids:
+    # c[fragments == j] = embeddings[np.where(node_ids == j)[0][0], i]
+
+    out_block = np.stack(out_channels)
     out_volume[block.write_roi] = out_block
 
 
 def relabel_fragments(file, ds, chunks_size, out_ds, node_ids, embeddings, num_workers):
     fragments = daisy.open_ds(file, ds)
 
-    if isinstance(chunks_size, int):
-        chunks_size = [chunks_size] * 3
-    block_size = daisy.Coordinate(chunks_size)
+    if len(chunks_size) == 1:
+        chunks_size = [chunks_size[0] for _ in range(3)]
+    block_size = daisy.Coordinate(chunks_size) * fragments.voxel_size
 
     out_volume = daisy.prepare_ds(
         filename=file,
@@ -97,11 +112,21 @@ def relabel_fragments(file, ds, chunks_size, out_ds, node_ids, embeddings, num_w
     )
 
 
+def rgb_ints_to_unit_floats(array):
+    pass
+
+
+def floats_to_rgb_ints(array):
+    return (minmax_scale(array, feature_range=(0, 1)) * 255).astype(np.uint64)
+
+
 if __name__ == "__main__":
     config = parse_args()
 
     node_ids, embeddings = load_embeddings(path=config.embeddings_path)
     embeddings_3d = pca(embeddings)
+    embeddings_3d = floats_to_rgb_ints(embeddings_3d)
+    logger.debug(f'embeddings_3d dtype {embeddings_3d.dtype}')
 
     relabel_fragments(
         file=config.fragments_zarr,
